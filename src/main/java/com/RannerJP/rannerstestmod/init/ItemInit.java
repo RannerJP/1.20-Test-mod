3bac6b7fbe219f02f97a6ed2da69e64472010095
Jose Pavon <RannerJP@gmail.com>
2024-04-04 06:01:23 -0700

    adds lime tools

--- src/main/java/com/RannerJP/rannerstestmod/init/ItemInit.java
+++ src/main/java/com/RannerJP/rannerstestmod/init/ItemInit.java
@@ -8,9 +8,14 @@ import static com.RannerJP.rannerstestmod.init.CreativeTabInit.addToTab;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.HoeItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.PickaxeItem;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ShovelItem;
+import net.minecraft.world.item.SwordItem;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -18,7 +23,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MODID);
 
-    public static final RegistryObject<Item> TEST_ITEM = addToTab(EXAMPLE_TAB_ITEMS, ITEMS.register("example_item", 
+    public static final RegistryObject<Item> LIME = addToTab(EXAMPLE_TAB_ITEMS, ITEMS.register("lime", 
     () -> new Item(new Item.Properties()
     .stacksTo(16)
     .food(new FoodProperties.Builder()
@@ -29,9 +34,47 @@ public class ItemInit {
     )));
 
     //Block Items
-    public static final RegistryObject<BlockItem> TEST_BLOCK_ITEM = addToTab(EXAMPLE_TAB_ITEMS, ITEMS.register("test_block", 
-        () -> new BlockItem(BlockInit.TEST_BLOCK.get(),
+    public static final RegistryObject<BlockItem> LIME_BLOCK_ITEM = addToTab(EXAMPLE_TAB_ITEMS, ITEMS.register("lime_block", 
+        () -> new BlockItem(BlockInit.LIME_BLOCK.get(),
                 new Item.Properties()
                     .rarity(Rarity.COMMON)
     )));
+
+    public static final RegistryObject<SwordItem> LIME_SWORD = addToTab(EXAMPLE_TAB_ITEMS, ITEMS.register("lime_sword", 
+        () -> new SwordItem(
+            TiersInit.LIME, 
+            7, 
+            1.1f,
+            new Item.Properties()
+        )));
+
+    public static final RegistryObject<PickaxeItem> LIME_PICKAXE = addToTab(EXAMPLE_TAB_ITEMS, ITEMS.register("lime_pickaxe", 
+        () -> new PickaxeItem(
+            TiersInit.LIME, 
+            7, 
+            1.1f,
+            new Item.Properties()
+        )));
+    public static final RegistryObject<ShovelItem> LIME_SHOVEL = addToTab(EXAMPLE_TAB_ITEMS, ITEMS.register("lime_shovel", 
+        () -> new ShovelItem(
+            TiersInit.LIME, 
+            7, 
+            1.1f,
+            new Item.Properties()
+        )));
+        
+    public static final RegistryObject<AxeItem> LIME_AXE = addToTab(EXAMPLE_TAB_ITEMS, ITEMS.register("lime_axe", 
+        () -> new AxeItem(
+            TiersInit.LIME, 
+            7, 
+            1.1f,
+            new Item.Properties()
+        )));
+    public static final RegistryObject<HoeItem> LIME_HOE = addToTab(EXAMPLE_TAB_ITEMS, ITEMS.register("lime_hoe", 
+        () -> new HoeItem(
+            TiersInit.LIME, 
+            7, 
+            1.1f,
+            new Item.Properties()
+        )));
 }
